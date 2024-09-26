c1b997cf4c8c03461656ebbd0caea9739cd0520b
Claude Rime <claude.rime@marvelous.digital>
2024-09-26 17:11:28 +0200

    added background image v2 asm

---
+++ c64-code/background-image/background-image-v2.asm
@@ -0,0 +1,42 @@
+; ----------------------------------------------------------
+; Basic header
+; ----------------------------------------------------------
+*=$0801  ; org
+
+.byte $0c,$08,$0a,$00,$9e,$20,$32
+.byte $30,$36,$34,$00,$00,$00,$00,$00 ; 15 bytes
+
+; ----------------------------------------------------------
+; Main program starts at $0810
+; ----------------------------------------------------------
+
+F_MAIN = $0810
+
+    LDX #$00
+    LDY #$00
+
+    ; Set up VIC-II for high-resolution bitmap mode
+    LDA #$1B         ; Set bitmap mode and high-resolution (Bit 5 set)
+    STA $D011        ; Enable bitmap mode
+    LDA #$18         ; Screen at $0400, bitmap at $2000
+    STA $D018
+
+    ; Copy bitmap data
+    LDA #<Bitmap_data
+    LDY #>Bitmap_data
+    JSR CopyBitmapData
+
+MainLoop
+    JMP MainLoop     ; Infinite loop
+
+CopyBitmapData
+    LDX #$00
+CopyLoop
+    LDA Bitmap_data, X
+    STA $2000, X
+    INX
+    BNE CopyLoop
+    RTS
+
+    ; Include bitmap data
+.include "bitmap-data.asm"
